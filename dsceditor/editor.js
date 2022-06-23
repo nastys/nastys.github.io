c77defe1dbd133b1e80c720539ba4bc230379f5d
nastys <nastys@users.noreply.github.com>
2022-06-23 14:31:46 +0200

    dsceditor dark mode support

--- dsceditor/editor.js
+++ dsceditor/editor.js
@@ -4,6 +4,7 @@ require(['vs/editor/editor.main'], function () {
     editor = monaco.editor.create(document.getElementById('container'), {
         value: ['PV_BRANCH_MODE(0);', 'TIME(0);', 'PV_END();', 'END();', ''].join('\n'),
         language: 'javascript',
-        automaticLayout: true
+        automaticLayout: true,
+        theme: browser_dark ? 'vs-dark' : 'vs-light',
     });
 });
